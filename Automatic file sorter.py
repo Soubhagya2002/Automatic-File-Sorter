2f9b2aee3875dfb1e210586e2aa0eaf56c07ca44
Soubhagya2002 <124516403+Soubhagya2002@users.noreply.github.com>
2024-03-08 22:07:03 +0530

    Update Automatic file sorter.py
    Sorts files into folders based on predefined rules or file attributes.

--- Automatic file sorter.py
+++ Automatic file sorter.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import os, shutil
-
-
-# In[2]:
-
 
 path= r"C:/Users/soubh/OneDrive/Documents/file sorter/"
 
-
-# In[3]:
-
-
 file_name = os.listdir(path)
-
-
-# In[4]:
-
 
 folder_names= ['csv files','image files','text files']
 
@@ -29,10 +14,6 @@ for loop in range(0,3):
         #print(path + folder_names[loop])
         os.makedirs(path + folder_names[loop])
 
-
-# In[5]:
-
-
 for file in file_name:
     if ".xlsx" in file and not os.path.exists(path + "csv files/" + file):
         shutil.move(path + file, path + "csv files/" + file)
@@ -40,23 +21,8 @@ for file in file_name:
         shutil.move(path + file, path + "image files/" + file)
     elif ".txt" in file and not os.path.exists(path + "csv files/" + file):
         shutil.move(path + file, path + "text files/" + file)
-    else:
-        print("There are files in this path that are not moved!")
+ 
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
